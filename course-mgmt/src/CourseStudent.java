72b71b2e8e3987e49e0675ae473869624f6d86c3
daleran <daleran@gmail.com>
2019-07-17 17:10:41 +0900

    Set up roster and course projects

---
+++ course-mgmt/src/CourseStudent.java
@@ -0,0 +1,15 @@
+/**
+ * SWENG 568
+ * RosterStudent.java
+ * Purpose: Plain Old Data Object simulating a student record
+ *
+ * @author Sean Davis
+ */
+public class CourseStudent {
+    //Public fields
+    public int StudentID;
+    public String StudentName;
+    public String StudentSSN;
+    public String StudentEmail;
+    public String StudentPhone;
+}
